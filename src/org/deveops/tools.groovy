c00c4f33f9cd307e25f2e1b8de11ce1a189c41cc
majiyue <43210949+majiyue@users.noreply.github.com>
2023-05-01 12:47:46 +0800

    Update tools.groovy

--- src/org/deveops/tools.groovy
+++ src/org/deveops/tools.groovy
@@ -1,7 +1,7 @@
 pacakge org.devops
 
 //带颜色打印信息
-def Post(value,color){
+def Printf(value,color){
     colors = ['red'   : "\033[40;31m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m",
               'blue'  : "\033[47;34m ${value} \033[0m",
               'green' : "\033[40;32m >>>>>>>>>>>${value}<<<<<<<<<<< \033[0m" ]
